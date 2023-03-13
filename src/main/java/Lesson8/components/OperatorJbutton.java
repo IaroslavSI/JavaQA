36ba16498a1e2875764d9775d906b1d0b6be325e
IaroslavS <iaroslavs@yandex.ru>
2023-03-13 19:06:31 +0500

    Выполнено Восьмое домашнее задание.

---
+++ src/main/java/Lesson8/components/OperatorJbutton.java
@@ -0,0 +1,12 @@
+package Lesson8.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OperatorJbutton extends JButton {
+    public OperatorJbutton(String text) {
+        super(text);
+        setFont(new Font("TimesRoman", Font.PLAIN, 30));
+        setBackground(new Color(255, 117, 0));
+    }
+}
